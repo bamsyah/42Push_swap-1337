ae4c3222bbf0bef5b47cd23d133b381dbe2f855c
Badreddine Msyah <badreddinemsyah@Badreddines-MacBook-Pro.local>
2023-12-05 00:32:04 +0100

    Finish

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bamsyah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 18:32:27 by bamsyah           #+#    #+#             */
-/*   Updated: 2023/12/04 23:47:31 by bamsyah          ###   ########.fr       */
+/*   Updated: 2023/12/05 00:30:30 by bamsyah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,9 @@ int	main(int ac, char **av)
 		algorithm(&stack_a, &stack_b);
 		print_list(stack_a);
 		free_list(&stack_a);
+		return (0);
 	}
 	// system("leaks push_swap");
+	ft_printf("Error\n");
 	return (0);
 }
